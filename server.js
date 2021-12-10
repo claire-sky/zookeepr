7f91277524d6c85e7e917a1301a483fbb751b5fc
Claire Sky <eclconnolly@gmail.com>
2021-12-10 12:59:31 -0800

    unique id

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals');
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray =[];
@@ -47,6 +52,14 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    // our function's main code will go here
+
+    // return finished code to post route for response
+    return body;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -62,6 +75,13 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
